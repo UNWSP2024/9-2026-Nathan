26915ffece20b2e3665f0b20d8fbf000804419c6
2026-Nathan <nnelsen@students.unwsp.edu>
2026-03-27 19:30:17 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -2,7 +2,6 @@
 # Written 3/27/26
 # Random Number File Writer
 
-# Program #2: Random Number File Writer
 # Write a program that writes a series of random numbers (up to 1000) to a file.
 # Each random number should be in the range of 1 through 500.
 # The application should let the user specify how many random numbers the file will hold
